df39644f8716bae022987e6ff5bb98b4365e3187
Pedro Ricardo <ordepricardo13@gmail.com>
2021-07-01 20:59:40 -0300

    first commit

---
+++ vue1/alurapic/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="body">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input class="filtro" type="search" v-model="filtro" placeholder="Buscar">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosFiltradas">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from './components/shared/painel/Painel.vue';
+
+export default {
+
+  components: {
+    'meu-painel': Painel,
+  },
+
+  data() {
+    return {
+      titulo: 'AluraPic',
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+    fotosFiltradas() {
+      const filtro = foto => foto.titulo.includes(this.filtro);
+      return this.fotos.filter(filtro);
+    }
+  },
+
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos, err => console.log(err));
+  },  
+}
+
+</script>
+
+<style>
+.centralizado {
+    text-align: center;
+}
+
+.body {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
